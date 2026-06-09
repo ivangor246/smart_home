04ab09d0be4dd9d0fe491cbcf4f9dcfba3465b06
ivangor246 <ivangor246@gmail.com>
2026-06-09 11:31:55 +0300

    feat: ws manager accepts topic now

--- src/app/ws/manager.py
+++ src/app/ws/manager.py
@@ -1,19 +1,21 @@
+from collections import defaultdict
+
 from fastapi import WebSocket
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active: set[WebSocket] = set()
+        self.active: dict[str, set[WebSocket]] = defaultdict(set)
 
-    async def connect(self, ws: WebSocket):
+    async def connect(self, topic: str, ws: WebSocket):
         await ws.accept()
-        self.active.add(ws)
+        self.active[topic].add(ws)
 
-    async def disconnect(self, ws: WebSocket):
-        self.active.discard(ws)
+    async def disconnect(self, topic: str, ws: WebSocket):
+        self.active[topic].discard(ws)
 
-    async def broadcast(self, data: dict):
-        for ws in set(self.active):
+    async def broadcast(self, topic: str, data: dict):
+        for ws in set(self.active[topic]):
             await ws.send_json(data)
 
 
